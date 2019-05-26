74670842ed9b97c0c3216f533485a741cd0f9573
Charlie Boyer <charlie.boyer@epitech.eu>
2019-05-26 19:26:31 +0200

    Retake my_str_to_word_array.c
    - Fix build warning and remove unused variables
    - Remove unused header
    * Test in removed main the function
    - Remove array NULL verification to fit 20 lines requirement (No impact here as long the set_array functions should return NULL to my_str_to_word_array which return the same state)
    
    Function Work !
    
    TODO : Managment \n presence

--- source/tools/my_str_to_word_array.c
+++ source/tools/my_str_to_word_array.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tools.h"
-#include "my_printf.h"
 #include "char_analyser_mode.h"
 
 int count_word(const char *str)
@@ -26,10 +25,9 @@ int count_word(const char *str)
     return (word);
 }
 
-char **set_array(const char *str)
+char **set_array(char *str)
 {
     int y = 0;
-    int i = 0;
     int word = count_word(str);
     char **array = malloc(sizeof(char *) * (word + 1));
 
@@ -46,16 +44,13 @@ char **set_array(const char *str)
     return (array);
 }
 
-char **my_str_to_word_array(const char *str)
+char **my_str_to_word_array(char *str)
 {
     int word_i = 0;
     int str_i = 0;
     int array_i = 0;
     char **array = set_array(str);
 
-    if (array == NULL) {
-        return (NULL);
-    }
     while (str[str_i] != '\0') {
         if (str[str_i] == ' ' || str[str_i] == '\t') {
             array[word_i][array_i] = '\0';
